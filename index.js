dd0eb59be6485fecf486ca0b6be51d7dc66c6328
Jordan <jnbarringer96@gmail.com>
2022-11-29 16:01:53 -0600

    pulled menu out of manager array into its own array and began the engineer question array

--- index.js
+++ index.js
@@ -3,34 +3,45 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('../utils/generateMarkdown');
 
-//Create an array of questions for user input
-const questions = [
-    //manager information
+//Create an array of questions for user input regarding manager information
+const start = [
     {
         type: 'input',
         name: 'manager-name',
-        message: "Please enter the team manager's name",
+        message: "Please enter the team manager's name.",
     },
     {
         type: 'input',
         name: 'manager-id',
-        message: "Please enter the team manager's ID",
+        message: "Please enter the team manager's ID.",
     },
     {
         type: 'input',
         name: 'email',
-        message: "Please enter the team manager's email",
+        message: "Please enter the team manager's email.",
     },
     {
         type: 'input',
         name: 'manager-number',
-        message: "Please enter the team manager's office number",
+        message: "Please enter the team manager's office number.",
     },
+]
+
     // add option to choose intern or engineer
+const menu = [
     {
         type: 'checkbox',
         name: 'intern-engineer-finish',
         message: "Add a team member or finish building your team.",
         choices: ['Add an engineer', 'Add an intern', 'Finish building your team']
     },
+]
+
+//Create an array of questions for user input regarding an engineer
+const engineer = [
+    {
+        type: 'input',
+        name: 'engineer-name',
+        message: "Please enter the engineer's name."
+    }
 ]
