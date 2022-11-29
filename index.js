624b3182b886ffe48df32985f528190e6fd0122b
Jordan <jnbarringer96@gmail.com>
2022-11-29 15:42:59 -0600

    added packages required for application

---
+++ index.js
@@ -0,0 +1,4 @@
+//Include Inquirer, file system, and generateMarkdown
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('../utils/generateMarkdown');
