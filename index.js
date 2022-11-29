91a35c9591db939a2f364f4d9852a3223241d516
Jordan <jnbarringer96@gmail.com>
2022-11-29 15:46:08 -0600

    added input directions for manager name

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('../utils/generateMarkdown');
+
+//Create an array of questions for user input
+const questions = [
+    //manager information
+    {
+        type: 'input',
+        name: 'manager-name',
+        message: "Please enter the team manager's name",
+    },
+]
