1fd57dce65e5148fc61caa8081e07906cd445d57
Jordan <jnbarringer96@gmail.com>
2022-12-03 11:30:23 -0600

    began writing tests for each employee type

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,8 @@
+const { it } = require("node:test");
+const { describe } = require("yargs");
+
+describe("Engineer", () => {
+    it("should", () => {
+
+    })
+})
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,8 @@
+const { it } = require("node:test");
+const { describe } = require("yargs");
+
+describe("Manager", () => {
+    it("should", () => {
+        
+    })
+})
