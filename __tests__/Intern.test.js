578a8a5fc2fd9e288e8cf49ef13bcddacc7c0d4c
Jordan <jnbarringer96@gmail.com>
2023-02-16 16:54:59 -0600

    finished intern jest

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,8 +1,10 @@
-const { it } = require("node:test");
-const { describe } = require("yargs");
+const Intern = require("../lib/Intern");
 
-describe("Intern", () => {
-    it("should", () => {
-        
-    })
+test("Ensures intern was created", () => {
+    const intern = new Intern("Lapis", 24, "idrathernotbehere@email.com", "gem school")
+
+    expect(intern.name).toEqual(expect.any(String));
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toEqual(expect.any(String));
+    expect(intern.school).toEqual(expect.any(String));
 })
