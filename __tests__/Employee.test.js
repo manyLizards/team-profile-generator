f297a1a7629e9925b7f8059c0f9d754e15f9d617
Jordan <jnbarringer96@gmail.com>
2023-03-14 18:02:36 -0500

    all tests are now passing

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,47 @@
-const { Employee } = require("../lib/Employee");
+const Employee = require("../lib/Employee");
 
 test("Ensures employee was created", () => {
-    const employee = new Employee("Peridot", 333, "techrules@gmail.com")
+    const employee = new Employee("Dale", 123, "test@test.com");
+    expect(typeof(employee)).toBe("object");
+});
 
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String));
+test("Can set name via constructor arguments", () => {
+    const name = "Alice";
+    const e = new Employee(name);
+    expect(e.name).toBe(name);
+});
+
+test("Can set id via constructor argument", () => {
+    const testValue = 100;
+    const e = new Employee("Foo", testValue, "test@email.com");
+    expect(e.id).toBe(testValue);
+});
+
+test("Can get email via constructor argument", () => {
+    const testValue = "test@test.com";
+    const e = new Employee ("Foo", 1, testValue);
+    expect(e.email).toBe(testValue);
+});
+
+test("Can get name via getName()", () => {
+    const testValue = "alice";
+    const e = new Employee(testValue);
+    expect(e.getName()).toBe(testValue);
+});
+
+test("Can get id via getId()", () => {
+    const testValue = 100;
+    const e = new Employee("Charlie", testValue, "test@test.com");
+    expect(e.getID()).toBe(testValue);
+});
+
+test("Can get email via getEmail()", () => {
+    const testValue = "test@test.com";
+    const e = new Employee("Charlie", 123, testValue);
+    expect(e.getEmail()).toBe(testValue);
+});
+
+test("Can get role via getRole()", () => {
+    const e = new Employee("Charlie", 123, "test@test.com");
+    expect(e.getRole()).toBe("Employee");
 })
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,10 +1,10 @@
 const Engineer = require("../lib/Engineer");
 
 test("Ensures engineer was initialized", () => {
-    const engineer = new Engineer("Steven", 3, "imacrystalgem@gmail.com", "www.github.com/mruniversejr")
+    const engineer = new Engineer("Steven", 3, "imacrystalgem@email.com", "www.github.com/mruniversejr")
 
     expect(engineer.name).toEqual(expect.any(String));
-    expect(engineer.id),toEqual(expect.any(Number));
-    expect(engineer.email).toEqual(expect.ay(String));
+    expect(engineer.id).toEqual(expect.any(Number));
+    expect(engineer.email).toEqual(expect.any(String));
     expect(engineer.github).toEqual(expect.any(String));
 })
